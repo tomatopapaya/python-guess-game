1ed37a183b231818632242a1523716bc20d89569
jerry <17app001@gmail.com>
2021-10-05 15:32:32 +0800

    新增提示目前區間大小

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 ### 進階功能
 - [x] 可讓玩家輸入題目區間
-- [] 提示目前區間大小
+- [x] 提示目前區間大小
 
 ### MarkDown
 - https://ithelp.ithome.com.tw/articles/10203758
--- guess-game.py
+++ guess-game.py
@@ -4,12 +4,16 @@ print('猜數字遊戲')
 start = int(input('start:'))
 end = int(input('end:'))
 count = int(input('count:'))
+low = start
+high = end
+
 x = random.randint(start, end)
 # print(x)
 for i in range(count):
     bingo = False
     while True:
         try:
+            print(f'{low}~{high}')
             y = int(input(f'第{i+1}/{count}次 請猜數字({start}~{end}):'))
             if x == y:
                 print('猜對了!')
@@ -18,9 +22,10 @@ for i in range(count):
 
             if x > y:
                 print('猜高一點~')
+                low = y+1 if y > low else low
             else:
                 print('猜低一點~')
-
+                high = y-1 if y < high else high
             break
         except:
             print('請輸入數字')
