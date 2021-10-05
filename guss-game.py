a7ad28364ef930b9c300e0821ca0f1a874f9ad47
jerry <17app001@gmail.com>
2021-10-05 14:28:51 +0800

    modify guess-game.py

--- guss-game.py
+++ guss-game.py
@@ -3,15 +3,31 @@ import random
 x = random.randint(1, 50)
 print(x)
 
+
 for i in range(5):
-    y = int(input('請猜數字(1~50)'))
-    if x == y:
-        print('猜對了!')
+    bingo = False
+    while True:
+        try:
+            y = int(input(f'第{i+1}/{5}次 請猜數字(1~50):'))
+            if x == y:
+                print('猜對了!')
+                bingo = True
+                break
+
+            if x > y:
+                print('猜高一點~')
+            else:
+                print('猜低一點~')
+
+            break
+
+        except Exception as e:
+            print('請輸入數字')
+
+    if bingo:
         break
 
-    if x > y:
-        print('猜高一點~')
-    else:
-        print('猜低一點~')
-
-print(f'答案為{x}')
+if bingo:
+    print('恭喜過關!')
+else:
+    print(f'答案為{x}')
